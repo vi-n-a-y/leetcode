7b103fca80f74aec3ac38a4de94ca984e98285a6
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-07-16 23:56:31 +0530

    Update BestTimetoBuyandSellStock.java

--- BestTimetoBuyandSellStock.java
+++ BestTimetoBuyandSellStock.java
@@ -1,26 +1,19 @@
 class BestTimetoBuyandSellStock{
-    public int maxProfit(int[] prices) {
-        int largePrice=0;
-        int smallerPrice=Integer.MAX_VALUE;
-        int day=0;
+  public int maxProfit(int[] prices) {
+    //    int smallerPrice=Integer.MAX_VALUE;
+        int small=0;
+       int  highest=0;
         for(int i=0;i<prices.length;i++){
-           if(prices[i]<smallerPrice){
-            smallerPrice=prices[i];
-            day=i;
-           }
-
-        }
-        if(smallerPrice==prices[prices.length-1]){
-            return 0;
-        }
-        for(int j=day+1;j<prices.length;j++){
-            if(prices[j]>largePrice){
-                largePrice=prices[j];
+            for(int j=i+1;j<prices.length;j++){
+                 small=prices[j]-prices[i];
+                 if(small>highest){
+                    highest=small;
+                 }
+               
+               
             }
-
-
+             System.out.println(highest);
         }
-      return largePrice-smallerPrice;
-        
+       return highest; 
     }
 }
